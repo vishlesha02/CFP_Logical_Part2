d02758756f6208bfaa60510aaac4b2424f87820a
vishlesha02 <vishleshamortale18@gmail.com>
2023-07-17 13:10:26 +0530

    Logical

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Basic basic=new Basic();
+
+            Console.WriteLine("Press 1 for Basic");
+            Console.WriteLine("press 8 to Exit");
+
+
+            int choice=int.Parse(Console.ReadLine());
+            
+                switch (choice)
+                {
+                    case 1:
+                    {
+                        Console.WriteLine("Enter your choice ");
+                        Console.WriteLine("Press 1 to  Add two numbers");
+
+
+                        int choice1 = int.Parse(Console.ReadLine());
+                        switch (choice1)
+                        {
+                            case 1:
+                                {
+                                   
+                                    break;
+                                }
+
+                            case 8:
+                                { 
+                                    break;
+                                }
+
+                            default: 
+                                {
+                                    break;
+                                }
+                        }
+                        break;
+
+                    }
+                default:
+                    {
+                        break;
+                    }
+
+                    
+               
+            
+                }
+
+
+
+
+        }
+    }
+}
